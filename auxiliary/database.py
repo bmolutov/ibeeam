025172a61292bb1d148ebd7543c3acc102522089
Bekzat Molutov <bekzat97plus@gmail.com>
2022-11-11 13:43:06 +0600

    async db settings added

---
+++ auxiliary/database.py
@@ -0,0 +1,6 @@
+import motor.motor_asyncio
+
+
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/')
+database = client.ibeeam
+collection = database.users
--- auxiliary/main.py
+++ auxiliary/main.py
@@ -2,11 +2,17 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from database import collection
+
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+async def read_root():
+    data = {
+        'first_name': 'bega'
+    }
+    await collection.insert_one(data)
     return {"Hello": "World"}
 
 
