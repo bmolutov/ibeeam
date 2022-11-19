4529141553a2170f071fe008ad0e56e1519ee27e
Bekzat Molutov <bekzat97plus@gmail.com>
2022-11-20 01:34:58 +0600

    final steps before deploy

--- ibeeam/ibeeam/settings.py
+++ ibeeam/ibeeam/settings.py
@@ -102,7 +102,8 @@ DATABASES = {
         'NAME': env('DATABASE_NAME'),
         'USER': env('DATABASE_USER'),
         'PASSWORD': env('DATABASE_PASSWORD'),
-        'HOST': env('DATABASE_HOST') if not DEBUG else 'localhost',
+        # 'HOST': env('DATABASE_HOST') if not DEBUG else 'localhost',
+        'HOST': env('DATABASE_HOST'),
         'PORT': env('DATABASE_PORT'),
     }
 }
